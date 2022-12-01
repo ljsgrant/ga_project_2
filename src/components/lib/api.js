7b56231644bd0edc5217b5f482b7661cc9398c29
Louis_Grant <ljsgrant@gmail.com>
2022-12-01 13:53:54 +0000

    fixed (again) function names in api.js

--- src/components/lib/api.js
+++ src/components/lib/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 // gets strCategory only
-export const getCategoriesNamesUrl = () =>
+export const getCategoryNames = () =>
   axios.get('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
 
 // gets idCategory, strCategory, strCategoryThumb, strCategoryDescription
-export const getCategoriesDetailsUrl = () =>
+export const getCategoryDetails = () =>
   axios.get('https://www.themealdb.com/api/json/v1/1/categories.php');
 
 // gets strMeal, strMealThumb, idMeal
