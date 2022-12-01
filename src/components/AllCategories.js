8869a0df408de5dcf42b2187d1c2e3df349820bf
Louis_Grant <ljsgrant@gmail.com>
2022-12-01 19:45:30 +0000

    working on categorycard, allcategories: cards show with data in allcategories

--- src/components/AllCategories.js
+++ src/components/AllCategories.js
@@ -1,5 +1,31 @@
+import { useState, useEffect } from 'react';
+import { getCategoryDetails } from './lib/api';
+import CategoryCard from './CategoryCard';
+
 const AllCategories = () => {
-  return <p>AllCategories</p>;
+  const [testCategories, setTestCategories] = useState(null);
+
+  useEffect(() => {
+    getCategoryDetails()
+      .then((response) => {
+        setTestCategories(response.data.categories);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  if (testCategories === null) {
+    return <p>Loading...</p>;
+  }
+
+  console.log(testCategories);
+
+  return (
+    <>
+      {testCategories.map((category) => (
+        <CategoryCard name={category.strCategory} imageUrl={category.strCategoryThumb} key={category.idCategory} />
+      ))}
+    </>
+  );
 };
 
 export default AllCategories;
