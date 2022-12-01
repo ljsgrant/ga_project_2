c66c6f1b8895e702d7a13ed0e9c656fad22d960e
Louis_Grant <ljsgrant@gmail.com>
2022-12-01 12:55:26 +0000

    finished endpoints in api.js

--- src/components/lib/api.js
+++ src/components/lib/api.js
@@ -8,3 +8,11 @@ export const CATEGORY_NAMES_URL = () =>
 export const CATEGORY_DETAILS_URL = () =>
   axios.get('https://www.themealdb.com/api/json/v1/1/categories.php');
 
+// gets strMeal, strMealThumb, idMeal
+export const FILTER_BY_CATEGORY_URL = (categoryName) =>
+  axios.get(
+    `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoryName}`
+  );
+
+export const SINGLE_MEAL_URL = (mealId) =>
+  axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`);
