13490a0fbad93d7b4193be7206061108137e9f7b
Louis_Grant <ljsgrant@gmail.com>
2022-12-01 13:38:27 +0000

    working on RecipeCard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import RecipeCard from './components/RecipeCard';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/recipe-card" element={<RecipeCard />} />
       </Routes>
     </BrowserRouter>
   );
