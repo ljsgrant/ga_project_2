accfd85367e89fe1bee5cbe93084fbfd42bacd4b
Louis_Grant <ljsgrant@gmail.com>
2022-12-01 16:55:04 +0000

    change naming convention to recipe rather than meal; styling tweaks to recipecard

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,47 +1,52 @@
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
+// import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { getSingleMeal } from './lib/api';
+import { getSingleRecipe } from './lib/api';
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-const RecipeCard = ({ name, imageUrl }) => {
-  const [testMeal, setTestMeal] = useState(null);
+const RecipeCard = ({ name, imageUrl, recipeId }) => {
+  const [testRecipe, setTestRecipe] = useState(null);
 
   useEffect(() => {
-    getSingleMeal('52772')
-      .then((response) => setTestMeal(response.data))
+    getSingleRecipe('52772')
+      .then((response) => setTestRecipe(response.data))
       .catch((err) => console.error(err));
   }, []);
 
-  if (testMeal === null) {
+  if (testRecipe === null) {
     return <p>Loading...</p>;
   }
 
   return (
     <>
       <Card sx={{ maxWidth: 400, borderRadius: '5px' }}>
-        <CardMedia
-          sx={{ maxHeight: 90 }}
-          component="img"
-          height="140"
-          image={testMeal.meals[0].strMealThumb}
-          alt="green iguana"
-        />
-        <CardContent sx={{ paddingBottom: 0 }}>
-          <Typography gutterBottom variant="h5" component="div">
-            {testMeal.meals[0].strMeal}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            <span>{testMeal.meals[0].strCategory}</span> ·{' '}
-            <span>{testMeal.meals[0].strArea}</span>
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Make this recipe</Button>
-        </CardActions>
+        <Link>
+          <CardMedia
+            sx={{ maxHeight: 90 }}
+            component="img"
+            height="140"
+            image={testRecipe.meals[0].strMealThumb}
+            alt="green iguana"
+          />
+          <CardContent sx={{ paddingBottom: 0 }}>
+            <Typography gutterBottom variant="h5" component="div">
+              {testRecipe.meals[0].strMeal}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              <span>{testRecipe.meals[0].strCategory}</span> ·
+              <span> {testRecipe.meals[0].strArea}</span> |
+              <span>
+                <Link size="small"> Make this recipe</Link>
+              </span>
+            </Typography>
+          </CardContent>
+          {/* <CardActions>
+        </CardActions> */}
+        </Link>
       </Card>
     </>
   );
