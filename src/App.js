d4a09e517cde3442d15504becfe61be30114cd47
elanieca <elanieca@hotmail.com>
2022-12-01 17:54:11 +0100

    set up all needed Routes with needed components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing from './components/Landing';
 import RecipeCard from './components/RecipeCard';
+import RecipeShow from './components/RecipeShow';
+import Category from './components/Category';
+import CategoryCard from './components/CategoryCard';
+import Categogies from './components/Categories';
 
 const App = () => {
   return (
@@ -8,6 +12,13 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/recipe-card" element={<RecipeCard />} />
+        <Route path="/category-card" element={<CategoryCard />} />
+        <Route
+          path="/categories/:category/:recipeId"
+          element={<RecipeShow />}
+        />
+        <Route path="/categories/:category" element={<Category />} />
+        <Route path="/categories" element={<Categories />} />
       </Routes>
     </BrowserRouter>
   );
