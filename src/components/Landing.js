4d201e4195aa9df47dcf405c981c75ab2eb416e4
elanieca <elanieca@hotmail.com>
2022-12-02 02:39:31 +0100

    build basic layout for Landing component and modified RecipeCard

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,27 @@
+import { useEffect, useState } from 'react';
+import { getCategoryNames } from '../lib/api';
+import CategoryHighlights from './CategoryHighlights';
+
 const Landing = () => {
-  return <p>landing</p>;
+  const [categories, setCategories] = useState(null);
+
+  useEffect(() => {
+    getCategoryNames()
+      .then(({ data }) => setCategories(data.meals))
+      .catch(({ response }) => console.error(response));
+  }, []);
+
+  if (categories === null) {
+    return <p>Loading content...</p>;
+  }
+
+  return (
+    <>
+      {categories.map((category) => (
+        <CategoryHighlights key={category.strCategory} {...category} />
+      ))}
+    </>
+  );
 };
 
 export default Landing;
