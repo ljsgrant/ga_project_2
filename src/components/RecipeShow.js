3ba0e8097574eaec0511045f4c0cdb7bcf4826cd
Louis_Grant <ljsgrant@gmail.com>
2022-12-03 20:11:27 +0000

    working on recipeshow: polishing up responsive layout

--- src/components/RecipeShow.js
+++ src/components/RecipeShow.js
@@ -7,6 +7,7 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import ImageDropdown from './ImageDropdown';
+import { Link } from 'react-router-dom';
 
 const RecipeShow = () => {
   const { recipeId } = useParams();
@@ -50,170 +51,204 @@ const RecipeShow = () => {
 
   return (
     <>
-      {/* Header */}
       <Box
         sx={{
-          display: 'flex',
-          '& > :not(style)': {
-            m: 1,
-            mb: 0.5
-            // width: '100%',
-            // height: 'auto'
-          }
+          width: '100%'
         }}
       >
-        <Paper
-          elevation={5}
+        {/* Header */}
+        <Box
           sx={{
-            backgroundImage: `radial-gradient(circle, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.2) ), url(${strMealThumb})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            color: 'white',
-            width: '100%',
             display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            textAlign: 'center',
-            height: {
-              xs: '80px',
-              sm: '300px',
-              md: '150px',
-              lg: '10px',
-              xl: '10px'
+            '& > :not(style)': {
+              mb: 1
+              // width: '100%',
+              // height: 'auto'
             }
           }}
         >
-          <Typography
-            variant="h3"
-            component="h2"
-            sx={{ textTransform: 'uppercase', fontWeight: 'bolder' }}
+          <Paper
+            elevation={5}
+            sx={{
+              backgroundImage: `radial-gradient(circle, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.2) ), url(${strMealThumb})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              color: 'white',
+              width: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              textAlign: 'center',
+              height: {
+                xs: '300px',
+                sm: '300px',
+                md: '190px',
+                lg: '90px',
+                xl: '90px'
+              }
+            }}
           >
-            {name}
-          </Typography>
-        </Paper>
-      </Box>
+            <Typography
+              variant="h3"
+              component="h2"
+              sx={{
+                textTransform: 'uppercase',
+                fontWeight: 'bolder',
+                fontSize: {
+                  xs: '200%',
+                  sm: '275%',
+                  md: '350%',
+                  lg: '400%',
+                  xl: '400%'
+                }
+              }}
+            >
+              {name}
+            </Typography>
+          </Paper>
+        </Box>
 
-      {/* Info (source etc) */}
-      <Box
-        sx={{
-          display: 'flex',
-          '& > :not(style)': {
-            m: 1,
-            mb: 0.5,
-            width: '100%'
-            // height: 'auto'
-          }
-        }}
-      >
-        <Paper
-          elevation={5}
+        {/* Info (source etc) */}
+        <Box
           sx={{
-            p: 1
+            display: 'flex',
+            '& > :not(style)': {
+              mb: 1,
+              width: '100%'
+              // height: 'auto'
+            }
           }}
         >
-          <Typography
-            variant="h7"
-            component="h3"
-            sx={{ mb: 0, textAlign: 'center' }}
-          >
-            Recipe from: {source}
-          </Typography>
-        </Paper>
-      </Box>
+          <a href={`${source}`}>
+            <Paper
+              elevation={5}
+              sx={{
+                p: 1,
+                backgroundColor: '#C17171',
+                color: 'white',
+                '&:hover': {
+                  backgroundColor: '#f7d2d2',
+                  color: 'black'
+                }
+              }}
+            >
+              <Typography
+                variant="h7"
+                component="h3"
+                sx={{ mb: 0, textAlign: 'center' }}
+              >
+                View recipe source &#8680;
+              </Typography>
+            </Paper>
+          </a>
+        </Box>
 
-      {/* Accordion */}
-      <Box
-        sx={{
-          display: 'flex',
-          '& > :not(style)': {
-            m: 1,
-            mb: 0.5,
-            width: '100%'
-            // height: 'auto'
-          }
-        }}
-      >
-        <Paper
-          elevation={5}
+        {/* Accordion */}
+        <Box
           sx={{
-            p: 0
+            display: 'flex',
+            '& > :not(style)': {
+              mb: 0.5,
+              width: '100%'
+              // height: 'auto'
+            }
           }}
         >
-          <ImageDropdown
-            strMealThumb={strMealThumb}
-            ingredients={ingredients}
-            strMeal={name}
-            strInstructions={strInstructions}
-          />
-        </Paper>
-      </Box>
+          <Paper
+            elevation={5}
+            sx={{
+              p: 0
+            }}
+          >
+            <ImageDropdown
+              strMealThumb={strMealThumb}
+              ingredients={ingredients}
+              strMeal={name}
+              strInstructions={strInstructions}
+            />
+          </Paper>
+        </Box>
 
-      {/* Main content */}
-      <Box
-        sx={{
-          display: {
-            xs: 'none',
-            sm: 'none',
-            md: 'flex',
-            lg: 'flex',
-            xl: 'flex'
-          },
-          minHeight: '100%',
-          flexDirection: {
-            xs: 'column',
-            sm: 'row',
-            md: 'row',
-            lg: 'row',
-            xl: 'row'
-          },
-          '& > :not(style)': {
-            m: 1
-          }
-        }}
-      >
-        <Paper
-          elevation={5}
+        {/* Main content */}
+        <Box
           sx={{
-            backgroundImage: `url(${strMealThumb})`,
-            backgroundPosition: 'center',
-            backgroundSize: 'cover',
-            m: 0,
-            width: '45vw',
-            height: '80vh',
             display: {
               xs: 'none',
               sm: 'none',
-              md: 'none',
+              md: 'flex',
               lg: 'flex',
               xl: 'flex'
-            }
-          }}
-          alt={`an image of ${name}.`}
-        />
-        <Paper elevation={5} sx={{ p: 5 }}>
-          <Typography variant="h5" component="h3" sx={{ mb: 1 }}>
-            Ingredients
-          </Typography>
-          {ingredients && <Ingredients ingredients={ingredients} />}
-        </Paper>
-        <Paper
-          elevation={5}
-          sx={{
-            p: 5,
-            flexBasis: {
-              xs: '80%',
-              sm: '90%',
-              md: '90%',
-              lg: 'flex',
-              xl: 'flex'
+            },
+            minHeight: '100%',
+            flexDirection: {
+              xs: 'column',
+              sm: 'row',
+              md: 'row',
+              lg: 'row',
+              xl: 'row'
+            },
+            '& > :not(style)': {
+              // m: 1
             }
           }}
         >
-          <Typography variant="h5" component="h3" sx={{ mb: 1 }}>
-            Method
-          </Typography>
-          {recipe && <Instructions strInstructions={strInstructions} />}
-        </Paper>
+          <Paper
+            elevation={5}
+            sx={{
+              backgroundImage: `url(${strMealThumb})`,
+              backgroundPosition: 'center',
+              backgroundSize: 'cover',
+              mr: 1,
+              width: '45vw',
+              height: '80vh',
+              display: {
+                xs: 'none',
+                sm: 'none',
+                md: 'none',
+                lg: 'flex',
+                xl: 'flex'
+              }
+            }}
+            alt={`an image of ${name}.`}
+          />
+          <Paper
+            elevation={5}
+            sx={{
+              p: 5,
+              mr: 1,
+              flexBasis: {
+                xs: '40%',
+                sm: '40%',
+                md: '40%',
+                lg: '40%',
+                xl: '30%'
+              }
+            }}
+          >
+            <Typography variant="h5" component="h3" sx={{ mb: 1 }}>
+              Ingredients
+            </Typography>
+            {ingredients && <Ingredients ingredients={ingredients} />}
+          </Paper>
+          <Paper
+            elevation={5}
+            sx={{
+              p: 5,
+              flexBasis: {
+                xs: '80%',
+                sm: '90%',
+                md: '90%',
+                lg: '90%',
+                xl: '90%'
+              }
+            }}
+          >
+            <Typography variant="h5" component="h3" sx={{ mb: 1 }}>
+              Method
+            </Typography>
+            {recipe && <Instructions strInstructions={strInstructions} />}
+          </Paper>
+        </Box>
       </Box>
     </>
   );
