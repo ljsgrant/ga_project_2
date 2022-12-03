d8233b89d5b1c5939bb3b768b89bf78c5cb2f161
elanieca <elanieca@hotmail.com>
2022-12-03 18:07:46 +0100

    added breakpoints for smaller screens and paper background

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,30 +1,14 @@
-import { Box, Grid } from '@mui/material';
-import { useEffect, useState } from 'react';
-import { getCategoryNames } from '../lib/api';
+import { Box } from '@mui/material';
 import CategoryHighlights from './CategoryHighlights';
 
-const Landing = () => {
-  const [categories, setCategories] = useState(null);
-
-  useEffect(() => {
-    getCategoryNames()
-      .then(({ data }) => setCategories(data.meals))
-      .catch(({ response }) => console.error(response));
-  }, []);
-
-  if (categories === null) {
-    return <p>Loading content...</p>;
-  }
-
-  return (
-    <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={3}>
-      {categories.map((category) => (
-        <Box gridColumn="span 12" key={category.strCategory} xs={12}>
-          <CategoryHighlights {...category} />
-        </Box>
-      ))}
-    </Box>
-  );
-};
+const Landing = ({ categories }) => (
+  <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={3}>
+    {categories.map((category) => (
+      <Box gridColumn="span 12" key={category.strCategory} xs={12}>
+        <CategoryHighlights {...category} />
+      </Box>
+    ))}
+  </Box>
+);
 
 export default Landing;
