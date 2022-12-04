a79ed5b5f01bc9ecac38e7e76fdda44d87e1b801
Louis_Grant <ljsgrant@gmail.com>
2022-12-04 18:25:46 +0000

    search works; needs fix for: currently fetches API data every time query changes

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,14 @@
-import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { getCategoryNames, getCategoryContents } from '../lib/api';
 import TextField from '@mui/material/TextField';
+import RecipeCard from './RecipeCard';
+import { Paper, Typography } from '@mui/material';
+import { Grid } from '@mui/material';
 
 const Search = () => {
   const [allCategories, setAllCategories] = useState([]);
   const [allRecipes, setAllRecipes] = useState(null);
-  const [searchString, setSearchString] = useState(null);
+  const [searchString, setSearchString] = useState('');
   const [matchingRecipes, setMatchingRecipes] = useState(null);
 
   useEffect(() => {
@@ -27,28 +29,60 @@ const Search = () => {
         });
       })
       .catch((err) => console.error(err));
-
-    console.log(allRecipes);
   }, [searchString]);
 
-  const handleChange = (event) => {
-    console.log('dsgdsgdg');
-    setSearchString(event.target.value);
+  const filterRecipes = () => {
+    const regex = new RegExp(searchString, 'i');
+    const filteredRecipes = allRecipes.filter((recipe) => {
+      return recipe.strMeal.match(regex);
+    });
+    // console.log(filteredRecipes);
+    return filteredRecipes;
   };
 
-  if (allRecipes !== null) {
-    console.log(allRecipes);
-  }
+  const handleChange = (event) => {
+    setSearchString(event.target.value);
+    console.log(event.target.value);
+  };
 
   return (
     <>
-      {searchString && <p>Searching for {searchString}</p>}
-      <TextField
-        onChange={handleChange}
-        fullWidth
-        label="Search for a recipe here..."
-        id="fullWidth"
-      />
+      {/* {allRecipes &&
+        filterRecipes().map((recipe) => (
+          <p key={recipe.strMeal}>{recipe.strMeal}</p>
+        ))} */}
+
+      <Paper
+        elevation={8}
+        sx={{ width: 1, padding: '15px', bgcolor: '#FAFAFA' }}
+      >
+        <TextField
+          value={searchString}
+          onChange={handleChange}
+          fullWidth
+          label="Search for a recipe here..."
+          id="fullWidth"
+          sx={{ mb: 2 }}
+        />
+
+        <Grid
+          container
+          spacing={1.5}
+          direction="row"
+          columns={{ xs: 3, sm: 6, md: 9, lg: 12 }}
+        >
+          {allRecipes && searchString &&
+            filterRecipes().map((recipe) => (
+              <Grid item key={recipe.strMeal} xs={3}>
+                <RecipeCard
+                  strMeal={recipe.strMeal}
+                  strMealThumb={recipe.strMealThumb}
+                  idMeal={recipe.idMeal}
+                />
+              </Grid>
+            ))}
+        </Grid>
+      </Paper>
     </>
   );
 };
